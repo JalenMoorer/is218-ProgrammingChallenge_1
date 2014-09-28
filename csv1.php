fa0b395864ac0af80373e87bf837a2184ca22016
jalen m moorer <jmm77@afsconnect1.njit.edu>
2014-09-28 15:05:10 -0400

    made seperate functions that reads any csv file and prints them

--- csv1.php
+++ csv1.php
@@ -3,15 +3,27 @@
 class csv
 {
 
-	public function __construct($csvfile){
+	public function __construct(){
 		echo "<h3>Reading from csvfile</h3>";	
 	}
 
-	public function read_file($csvfile)
+	public function csv_print()
+	{
+		$records = $this->csv_read("test.csv");
+
+		foreach($records as $record){
+			foreach($record as $key => $value) {
+				echo "<b>$key</b>". ': ' . $value .  "</br> \n";
+			}echo '<hr>';
+		}
+	}
+
+	public function csv_read($csvfile)
 	{	
 		$firstrow = TRUE;
 		ini_set('auto_detect_line_endings', TRUE);
-		if (( $handle = fopen($csvfile, "r")) !== FALSE) {
+		if (( $handle = fopen($csvfile, "r")) !== FALSE)
+		/*fgetcsv($handle, 1000, ",");*/ {
 			while (($row = fgetcsv($handle, 1000, ",")) !== FALSE){
 			if($firstrow == TRUE ){
 			$column_heading = $row;
@@ -24,14 +36,8 @@ class csv
 		 	}
 			fclose($handle);
 		}
-		foreach($records as $record){
-			foreach($record as $key => $value) {
-				echo "<b>$key</b>". ': ' . $value .  "</br> \n";
-				//format web key?
-			}echo '<hr>';
-		}
+		return $records;
 	}
-	
 	
 	public function __destruct(){
 		echo "<h3>Done reading from csvfile</h3>";
@@ -40,13 +46,7 @@ class csv
 
 
 $csv_obj = new csv;
-$csv_obj->read_file("test.csv");
-
-//$csv_obj2 = new csv;
-//$csv_obj2->read_file("countrylist.csv");
-
-
-
+$csv_obj->csv_print();
 
 
 ?>
