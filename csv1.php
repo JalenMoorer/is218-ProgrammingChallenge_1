5f923991e1ff91a10e3c37c589537335437f706a
jalen m moorer <jmm77@afsconnect1.njit.edu>
2014-10-02 20:02:14 -0400

    Changed class functions to static and refactored some code

--- csv1.php
+++ csv1.php
@@ -1,36 +1,29 @@
 <?php
 
-error_reporting(E_ERROR | E_PARSE); //Only disable warnings to view page clearly!
+//error_reporting(E_ERROR | E_PARSE); //Only disable warnings to view page clearly!
 
 class csv
 {
+	public $colleges;
+	public $full_titles;
 
 	public function __construct(){
 		echo "<body style='margin: 0; padding:0;'>";
 		echo "<h3>Integrated Post Secondary Educational Data System</h3>";	
 	}
 
-	public function csv_print()
+	public static function csv_print_college($records, $dictionary)
 	{
-		$records = $this->csv_read("hd2013.csv");
-		$dictionary = $this->csv_read("dictionary.csv");
-
-		//$num = count($records);
-		//echo $records[0]['CHFNM'];
 
 		$colleges = array_column($records, 'INSTNM', 'UNITID');
 		$full_titles = array_column($dictionary, 'varTitle', 'varnumber');
-
-		//print_r($colleges);
-		//print_r($full_title);
 
 		$i = 0;
 		if(empty($_GET)){
 		foreach($colleges as $key => $value){
 			$i++;
 			$college_number = $i - 1;
-			echo '<a href=' . '"http://localhost/php/csv1.php?record='. $college_number . '"' . '>' . $value . '</a><br>'; 
-			echo '</p>';
+			csv::create_link($college_number, $value);
 			$college_name = $value;
 		}
 	  }
@@ -39,10 +32,15 @@ class csv
 		$full_title = $full_titles[$_GET['record']];
 		$college_title = $college_name[$_GET['record']];
 
+		csv::csv_print_records($record, $full_title, $full_titles, $college_title, $colleges);
+	}
+
+	public static function csv_print_records($record, $full_title, $full_titles, $college_title, $colleges)
+	{
 		$table_head = TRUE;
 		foreach($full_titles as $key => $value){
 			if($table_head == TRUE)
-			{	
+			{	//Make html stuff here static
 				echo "<table style='text-align: center;' border=\"1\"><tr>"; $j++;
 				$table_head = FALSE;
 			}
@@ -53,10 +51,15 @@ class csv
 			echo "<td>".$value."</td>";
 		}
 		echo "</table>";
-
 	}
 
-	public function csv_read($csvfile)
+	public static function create_link($college_number, $value)
+	{
+		echo '<a href=' . '"http://localhost/php/csv1.php?record='. $college_number . '"' . '>' . $value . '</a><br>'; 
+		echo '</p>';
+	}
+
+	public static function csv_read($csvfile)
 	{	
 		$firstrow = TRUE;
 		ini_set('auto_detect_line_endings', TRUE);
@@ -76,10 +79,14 @@ class csv
 		return $records;
 	}
 }
+	
+$csvfile1 = "hd2013.csv"; 
+$csvfile2 = "dictionary.csv";
 
+$records = csv::csv_read($csvfile1);
+$dictionary = csv::csv_read($csvfile2);
 
-$csv_obj = new csv;
-$csv_obj->csv_print();
+csv::csv_print_college($records, $dictionary);
 
 
 ?>
