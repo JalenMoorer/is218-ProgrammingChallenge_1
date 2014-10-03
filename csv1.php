bfd04101df1be2b247a9811abd8fab2b0fc3b1c0
jalen m moorer <jmm77@afsconnect1.njit.edu>
2014-10-02 20:22:30 -0400

    Added comments and changed the hyperlink to be able to load on AFS

--- csv1.php
+++ csv1.php
@@ -1,6 +1,9 @@
 <?php
-
-//error_reporting(E_ERROR | E_PARSE); //Only disable warnings to view page clearly!
+/*
+@author Jalen Moorer
+IS218 - Programming Challenge #1
+October 3rd, 2014
+*/
 
 class csv
 {
@@ -14,33 +17,35 @@ class csv
 
 	public static function csv_print_college($records, $dictionary)
 	{
-
+		//Split column values from both arrays and stored them into new arrays to use
 		$colleges = array_column($records, 'INSTNM', 'UNITID');
 		$full_titles = array_column($dictionary, 'varTitle', 'varnumber');
+
 
 		$i = 0;
 		if(empty($_GET)){
 		foreach($colleges as $key => $value){
 			$i++;
 			$college_number = $i - 1;
-			csv::create_link($college_number, $value);
+			csv::create_link($college_number, $value); //call the function to print out each college record and the hyperlink for each
 			$college_name = $value;
 		}
 	  }
 
-		$record = $records[$_GET['record']];
+		$record = $records[$_GET['record']]; 
 		$full_title = $full_titles[$_GET['record']];
 		$college_title = $college_name[$_GET['record']];
 
-		csv::csv_print_records($record, $full_title, $full_titles, $college_title, $colleges);
+		csv::csv_print_records($record, $full_title, $full_titles, $college_title, $colleges); //call the function to print out the _GET values from both arrays
 	}
 
 	public static function csv_print_records($record, $full_title, $full_titles, $college_title, $colleges)
 	{
 		$table_head = TRUE;
+		$j = 0;
 		foreach($full_titles as $key => $value){
-			if($table_head == TRUE)
-			{	//Make html stuff here static
+			if($table_head == TRUE) 
+			{	//prints out all records in HTML Table format
 				echo "<table style='text-align: center;' border=\"1\"><tr>"; $j++;
 				$table_head = FALSE;
 			}
@@ -48,19 +53,21 @@ class csv
 		}
 		echo "</tr>";
 		foreach($record as $key => $value){
-			echo "<td>".$value."</td>";
+			echo "<td>".$value."</td>"; 
 		}
 		echo "</table>";
 	}
 
 	public static function create_link($college_number, $value)
 	{
-		echo '<a href=' . '"http://localhost/php/csv1.php?record='. $college_number . '"' . '>' . $value . '</a><br>'; 
+		//creates a link in html format containing the index value of needed to print out the dictionary and college records in detail
+		echo '<a href=' . '"http://web.njit.edu/~jmm77/is218-fall2014/csv1.php?record='. $college_number . '"' . '>' . $value . '</a><br>'; 
 		echo '</p>';
 	}
 
 	public static function csv_read($csvfile)
 	{	
+		//reads from a csv file
 		$firstrow = TRUE;
 		ini_set('auto_detect_line_endings', TRUE);
 		if (( $handle = fopen($csvfile, "r")) !== FALSE) {
@@ -69,7 +76,7 @@ class csv
 			$column_heading = $row;
 			$firstrow = FALSE;	
 			}else{
-			$record = array_combine($column_heading, $row);
+			$record = array_combine($column_heading, $row); //turns the first column into keys and the rest of the values uner it as values
 			$records[] = $record;				
 			}
 				
@@ -79,14 +86,16 @@ class csv
 		return $records;
 	}
 }
-	
-$csvfile1 = "hd2013.csv"; 
+
+$csv_object = new csv; //instantiated an object to invoke the constructor 
+
+$csvfile1 = "hd2013.csv";   
 $csvfile2 = "dictionary.csv";
 
-$records = csv::csv_read($csvfile1);
-$dictionary = csv::csv_read($csvfile2);
+$records = csv::csv_read($csvfile1); //store the contents of both files into seperate 
+$dictionary = csv::csv_read($csvfile2);//arrays that will be used for printing
 
-csv::csv_print_college($records, $dictionary);
+csv::csv_print_college($records, $dictionary); // call the print function to print contents from both arrays
 
 
 ?>
