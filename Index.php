f022c185e22efac8737cdec56fcd3fe5ab881cc1
jalen m moorer <jmm77@afsconnect1.njit.edu>
2014-10-15 23:19:01 -0400

    Added some html headers on Index.php

--- Index.php
+++ Index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Programming Challenge #1</title>
+</head>
+<body>
+
+<h1> Integrated Post Secondary Educational Data System </h1>
+<hr>
 <?php
 
 require_once('Classes/Autoloader.php');
@@ -12,7 +21,8 @@ $menu = new Classes\csv\menu();
 
 $menu->menu_links($hd2013, $dictionary); //Creates a list of colleges on the Index Page
 
-
-
-
 ?>
+
+</body>
+</html>
+
